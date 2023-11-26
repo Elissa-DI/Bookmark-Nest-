bee78fd7496a0baa97cfcab9d6891dbd2eb12de3
Elissa <elissadusabe@gmail.com>
2023-11-26 16:19:05 +0200

    added providers, modules and controllers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,13 @@
 /* eslint-disable prettier/prettier */
-import { Module } from '@nestjs/common';
+import { Module, Post } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
-@Module({}) 
-export class AuthModule {}
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService]  
+}) 
+export class AuthModule {
+    constructor(private authService: AuthService) {}
+
+}
